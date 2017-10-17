abd36353c4d523e93a8ea828e60b39b8cb3ac32f
Adrian Schatzmann <adimail@hispeed.ch>
2017-10-17 22:33:14 +0200

    Methods investigated

--- Decrypt.Test/DeflatTest.cs
+++ Decrypt.Test/DeflatTest.cs
@@ -7,18 +7,38 @@ namespace Decrypt.Test
     public class DeflatTest
     {
         [TestMethod]
+        public void Compress()
+        {
+            using (var streamUncompressed = new System.IO.FileStream(@"C:\Users\Adrian\Downloads\2008_civic_sedan.pdf", System.IO.FileMode.Open))
+            using (var streamCompressed = new System.IO.FileStream(@"C:\Users\Adrian\Downloads\2008_civic_sedan.z", System.IO.FileMode.Create))
+            {
+                var deflateStream = new System.IO.Compression.DeflateStream(streamCompressed, System.IO.Compression.CompressionMode.Compress);
+                streamUncompressed.CopyTo(deflateStream);
+            }
+        }
+
+        [TestMethod]
+        public void Decompress()
+        {
+            using (var streamUncompressed = new System.IO.FileStream(@"C:\Users\Adrian\Downloads\7z1604-x64_2.exe", System.IO.FileMode.Create))
+            using (var streamCompressed = new System.IO.FileStream(@"C:\Users\Adrian\Downloads\7z1604-x64.z", System.IO.FileMode.Open))
+            {
+                var deflateStream = new System.IO.Compression.DeflateStream(streamCompressed, System.IO.Compression.CompressionMode.Decompress);
+                deflateStream.CopyTo(streamUncompressed);
+            }
+        }
+
+        [TestMethod]
         public void TestMethod1()
         {
-            byte[] data = new byte[] { 0x03, 0x20, 0x04, 0x06, 0x04, 0xeb, 0x2d, 0x52, 0xdf, 0xa3, 0x5f, 0x87, 0x30, 0x71, 0x09, 0x49, 0x20, 0x26, 0x0d, 0x38, 0x3c, 0xba, 0x98, 0xf3, 0x01, 0x00, 0x01, 0x00, 0x4f, 0x81, 0xc9, 0x05, 0xbf, 0xfb, 0x38, 0xf6, 0xbe, 0x5b, 0x42, 0x78, 0x59, 0xbb, 0x2b, 0x6c, 0x4e, 0x60, 0xce, 0x8b, 0xde, 0xcd, 0x62, 0xab, 0x44, 0xb6, 0xa9, 0x85, 0x4b, 0xf6, 0x62, 0xd2};
+            byte[] data = new byte[] { 0x74, 0x11, 0x09, 0x41, 0x04, 0xb9, 0xd4, 0xfe, 0x4d, 0x5f, 0x9d, 0x84, 0x71, 0x23, 0x73, 0x7b, 0x9c, 0x42, 0x63, 0xf0, 0x0c, 0x3b, 0x45, 0xf2, 0x43, 0xb8, 0xd8, 0xfa, 0x94, 0xa6, 0xe2, 0x69, 0xf5, 0x3b, 0xd2, 0xb6, 0xa3, 0x5a, 0x1c, 0x60, 0x0f, 0x45, 0x84, 0x84, 0x50, 0x80, 0x20, 0x71 };
+
             var streamCompressed = new System.IO.MemoryStream(data);
-            //var streamDecompressed = new System.IO.MemoryStream();
+            var streamDecompressed = new System.IO.MemoryStream();
             
 
             var deflateStream = new System.IO.Compression.DeflateStream(streamCompressed, System.IO.Compression.CompressionMode.Decompress);
-            //deflateStream.CopyTo(streamDecompressed);
-
-            byte[] buffer = new byte[100];
-            int bytesRead = deflateStream.Read(buffer, 0, buffer.Length);
+            deflateStream.CopyTo(streamDecompressed);
         }
     }
 }
