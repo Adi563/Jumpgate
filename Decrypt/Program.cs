adca3116e9292fdb03f83bf7a375e9721c3da7e6
Adrian Schatzmann <adrian.schatzmann@students.fhnw.ch>
2017-11-03 14:30:45 +0100

    Reflection key modification added

--- Decrypt/Program.cs
+++ Decrypt/Program.cs
@@ -28,6 +28,8 @@
             aes.GenerateIV();
             aes.Padding = System.Security.Cryptography.PaddingMode.None;
 
+            var decryptor = aes.CreateDecryptor();
+
             var encryptedData = new byte[] { 0x06, 0xa3, 0xa1, 0xab, 0x1e, 0x10, 0x9c, 0xa9, 0x40, 0x14, 0xd7, 0x2f, 0x40, 0xdf, 0x51, 0x73, 0x63, 0xf3, 0x08, 0xa9, 0x47, 0x10, 0xe0, 0x03, 0xe3, 0xd8, 0x6a, 0x35, 0x22, 0x7c, 0x28, 0x95, 0xce, 0xd5, 0xbe, 0x72, 0x8d, 0x04, 0xfe, 0x26, 0x9b, 0x60, 0xa9, 0xf6, 0x0a, 0xc8, 0x87, 0x5d };
             var decryptedData = new byte[encryptedData.Length];
 
@@ -60,9 +62,21 @@
                 Number1Incremented:
 
                 aes.Key = key;
-                var decryptor = aes.CreateDecryptor();
-                decryptor.TransformBlock(encryptedData, 0, encryptedData.Length, decryptedData, 0);
 
+                //var fieldAesKey = aes.GetType().GetField("m_key", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                //var mKeyAes = fieldAesKey.GetValue(aes);
+                //var fieldAesHandle = fieldAesKey.FieldType.GetField("handle", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                //var pointerAes = fieldAesHandle.GetValue(mKeyAes);
+
+                //var fieldDecryptorKey = decryptor.GetType().GetField("m_key", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                //var mKeyDecryptor = fieldDecryptorKey.GetValue(decryptor);
+                //var fieldDecryptorHandle = fieldDecryptorKey.FieldType.GetField("handle", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                //fieldDecryptorHandle.SetValue(mKeyDecryptor, pointerAes);
+
+                decryptor = aes.CreateDecryptor();
+
+                decryptor.TransformBlock(encryptedData, 0, encryptedData.Length, decryptedData, 0);
+                
                 if (decryptedData[45] == 'x' && decryptedData[46] == 'x' && decryptedData[47] == 'x')
                 { System.Console.WriteLine(System.Text.Encoding.ASCII.GetString(decryptedData)); }
             }
