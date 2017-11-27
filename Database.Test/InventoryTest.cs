df7971cc8ca1a9ec5e0b3a829dca43601b9e693f
Adrian Schatzmann <adimail@hispeed.ch>
2017-11-27 22:31:31 +0100

    Inventory added

---
+++ Database.Test/InventoryTest.cs
@@ -0,0 +1,43 @@
+﻿namespace Jumpgate.Database.Test
+{
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class InventoryTest
+    {
+        [TestMethod]
+        public void GetInventory()
+        {
+            var jsonData =  new System.Net.WebClient().DownloadData("http://jumpgate-tri.org/jossh-api/stations-inventory.json");
+
+            Item[] items;
+
+            var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(Database.Item[]));
+            using (var stream = new System.IO.MemoryStream(jsonData))
+            {
+                items = (Item[])serializer.ReadObject(stream);
+            }
+
+            var itemsCommodities = items.Where(i => i.GroupName.Equals("Commodities"));
+            var itemsGroupedByStations = itemsCommodities.GroupBy(i => i.StationName);
+
+            var currentStation = "Solrain Wake";
+            var itemsForCurrentStation = itemsGroupedByStations.First(ig => ig.Key.Equals(currentStation));
+
+            Item itemBest = null;
+            int differenceBest = 0;
+            foreach (var itemForCurrentStation in itemsForCurrentStation)
+            {
+                var itemsForOtherStation = itemsCommodities.Where(i => !i.StationName.Equals(currentStation) && i.Id == itemForCurrentStation.Id);
+                
+                var itemWithBestDifference = itemsForOtherStation.OrderByDescending(i => (int)i.Price - (int)itemForCurrentStation.Price).First();
+                
+                if ((int)itemWithBestDifference.Price - (int)itemForCurrentStation.Price < differenceBest) { continue; }
+                itemBest = itemWithBestDifference;
+                differenceBest = (int)itemWithBestDifference.Price - (int)itemForCurrentStation.Price;
+            }
+        }
+    }
+}
---
+++ Database.Test/Item.cs
@@ -0,0 +1,19 @@
+﻿namespace Jumpgate.Database
+{
+    [System.Runtime.Serialization.DataContract]
+    public class Item
+    {
+        [System.Runtime.Serialization.DataMember(EmitDefaultValue = false, IsRequired = true, Name = "item_id", Order = 1)]
+        public uint Id { get; set; }
+        [System.Runtime.Serialization.DataMember(EmitDefaultValue = false, IsRequired = true, Name = "name", Order = 2)]
+        public string Name { get; set; }
+        [System.Runtime.Serialization.DataMember(EmitDefaultValue = false, IsRequired = true, Name = "price", Order = 3)]
+        public uint Price { get; set; }
+        [System.Runtime.Serialization.DataMember(EmitDefaultValue = false, IsRequired = true, Name = "amount", Order = 4)]
+        public uint Amount { get; set; }
+        [System.Runtime.Serialization.DataMember(EmitDefaultValue = false, IsRequired = true, Name = "station_name", Order = 5)]
+        public string StationName { get; set; }
+        [System.Runtime.Serialization.DataMember(EmitDefaultValue = false, IsRequired = true, Name = "group_name", Order = 6)]
+        public string GroupName { get; set; }
+    }
+}
