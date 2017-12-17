a71f215992183773aa1c850893d93baed051097d
Adrian Schatzmann <adimail@hispeed.ch>
2017-12-17 23:35:08 +0100

    Refactoring

--- Database.Test/InventoryTest.cs
+++ Database.Test/InventoryTest.cs
@@ -8,12 +8,86 @@
     public class InventoryTest
     {
         [TestMethod]
-        public void GetInventory()
+        public void GetDeliveries()
         {
-            //var jsonData =  new System.Net.WebClient().DownloadData("http://jumpgate-tri.org/jossh-api/stations-inventory.json");
-            var streamJson = this.GetType().Assembly.GetManifestResourceStream("Database.Test.stations-inventory.json");
-            var jsonData = new byte[streamJson.Length];
-            streamJson.Read(jsonData, 0, jsonData.Length);
+            var items = GetInventory();
+
+            var itemsForCurrentStation = FilterItems(items, "Solrain Wake");
+            var itemsForOtherStations = FilterItems(items);
+
+            var deliveriesOrderedByProfit = GetDeliveriesOrderedByProfit(itemsForCurrentStation, itemsForOtherStations, 499, 1.0092938888m);
+
+            System.Diagnostics.Debug.WriteLine("Name\tAmount\tPrice\tFromStationName\tToStationName\tProfit");
+
+            foreach (var delivery in deliveriesOrderedByProfit.Where(d => d.Profit > 0))
+            {
+                //System.Diagnostics.Debug.WriteLine($"Item: {delivery.To.Name} ({delivery.Amount}) ({delivery.From.StationName} -> {delivery.To.StationName}) - Profit: {delivery.Profit}");
+                System.Diagnostics.Debug.WriteLine($"{delivery.To.Name}\t{delivery.Amount}\t{delivery.Price}\t{delivery.From.StationName}\t{delivery.To.StationName}\t{delivery.Profit}");
+            }
+        }
+
+
+        [TestMethod]
+        public void GetTaxPercentByPoliticalRatingTest()
+        {
+            var tax = GetTaxPercentByPoliticalRating(75);
+        }
+
+
+        /// <summary>
+        /// Filters the items.
+        /// </summary>
+        /// <param name="items">The items.</param>
+        /// <param name="stationName">Name of the station.</param>
+        /// <returns></returns>
+        private static System.Collections.Generic.IEnumerable<Item> FilterItems(System.Collections.Generic.IEnumerable<Item> items, string stationName)
+        {
+            return items.Where(i => i.GroupName.Equals("Commodities")).Where(i => i.StationName.Equals("Solrain Wake"));
+        }
+
+
+        /// <summary>
+        /// Filters the items.
+        /// </summary>
+        /// <param name="items">The items.</param>
+        /// <returns></returns>
+        private static System.Collections.Generic.IEnumerable<Item> FilterItems(System.Collections.Generic.IEnumerable<Item> items)
+        {
+            return items.Where(i => i.GroupName.Equals("Commodities"));
+        }
+
+
+        /// <summary>
+        /// Gets the deliveries ordered by profit.
+        /// </summary>
+        /// <param name="itemsForCurrentStation">The items for current station.</param>
+        /// <param name="itemsForOtherStations">The items for other stations.</param>
+        /// <param name="cargoSpace">The cargo space.</param>
+        /// <param name="taxRate">The tax rate.</param>
+        /// <returns></returns>
+        private static System.Collections.Generic.IEnumerable<Delivery> GetDeliveriesOrderedByProfit(System.Collections.Generic.IEnumerable<Item> itemsForCurrentStation, System.Collections.Generic.IEnumerable<Item> itemsForOtherStations, ushort cargoSpace, decimal taxRate)
+        {
+            decimal taxRateRounded = Math.Round(taxRate, 4);
+            
+            var deliveries = itemsForCurrentStation.SelectMany(
+                itemForCurrentStation => itemsForOtherStations.Where(itemForOtherStation => itemForCurrentStation.Id == itemForOtherStation.Id && !itemForCurrentStation.StationName.Equals(itemForOtherStation.StationName)).Select(itemForOtherStation => new Delivery(itemForCurrentStation, itemForOtherStation, Math.Min(cargoSpace, itemForCurrentStation.Amount), taxRateRounded)));
+
+            var deliveriesOrderedByProfit = deliveries.OrderByDescending(delivery => delivery.Profit);
+
+            return deliveriesOrderedByProfit;
+        }
+
+
+        /// <summary>
+        /// Gets the inventory.
+        /// </summary>
+        /// <returns></returns>
+        private static System.Collections.Generic.IEnumerable<Item> GetInventory()
+        {
+            var jsonData = new System.Net.WebClient().DownloadData("http://jumpgate-tri.org/jossh-api/stations-inventory.json");
+            //var streamJson = typeof(InventoryTest).Assembly.GetManifestResourceStream("Database.Test.stations-inventory.json");
+            //var jsonData = new byte[streamJson.Length];
+            //streamJson.Read(jsonData, 0, jsonData.Length);
             Item[] items;
 
             var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(Jumpgate.Database.Test.Item[]));
@@ -22,36 +96,15 @@
                 items = (Item[])serializer.ReadObject(stream);
             }
 
-            var itemsCommodities = items.Where(i => i.GroupName.Equals("Commodities"));
-            var itemsGroupedByStations = itemsCommodities.GroupBy(i => i.StationName);
-
-            ushort cargoSpace = 499;
-            decimal taxRate = 1.0092938888m;
-            decimal taxRateRounded = Math.Round(taxRate, 4);
-
-            var itemsForCurrentStation = itemsCommodities.Where(i => i.StationName.Equals("Solrain Wake"));
-            var itemsForOtherStations = itemsCommodities;
-
-            var deliveries = itemsForCurrentStation.SelectMany(
-                itemForCurrentStation => itemsForOtherStations.Where(itemForOtherStation => itemForCurrentStation.Id == itemForOtherStation.Id && !itemForCurrentStation.StationName.Equals(itemForOtherStation.StationName)).Select(itemForOtherStation => new Delivery(itemForCurrentStation, itemForOtherStation, Math.Min(cargoSpace, itemForCurrentStation.Amount), taxRateRounded)));
-
-            var deliveriesOrderedByPrice = deliveries.OrderByDescending(delivery => delivery.Profit);
-
-            System.Diagnostics.Debug.WriteLine("Name\tAmount\tPrice\tFromStationName\tToStationName\tProfit");
-
-            foreach (var delivery in deliveriesOrderedByPrice.Where(d => d.Profit > 0))
-            {
-                //System.Diagnostics.Debug.WriteLine($"Item: {delivery.To.Name} ({delivery.Amount}) ({delivery.From.StationName} -> {delivery.To.StationName}) - Profit: {delivery.Profit}");
-                System.Diagnostics.Debug.WriteLine($"{delivery.To.Name}\t{delivery.Amount}\t{delivery.Price}\t{delivery.From.StationName}\t{delivery.To.StationName}\t{delivery.Profit}");
-            }
+            return items;
         }
 
-        [TestMethod]
-        public void GetTaxPercentByPoliticalRatingTest()
-        {
-            var tax = GetTaxPercentByPoliticalRating(75);
-        }
 
+        /// <summary>
+        /// Gets the tax percent by political rating.
+        /// </summary>
+        /// <param name="politicalRating">The political rating.</param>
+        /// <returns></returns>
         private static decimal GetTaxPercentByPoliticalRating(sbyte politicalRating)
         {
             return -0.02m * politicalRating + 2;
